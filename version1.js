b7c77ca24ce996c165e370aee941a904cd475418
rociobastiasanchez <rociobastiasanchez@gmail.com>
2021-02-14 19:07:22 +0100

    actualizar

---
+++ version1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// document.body.innerHTML
+// let botonmenu = $('.iconomenu');
+// let menu = $ ('.caja1');
+
+// botonmenu.click(function(){
+//     if (menu.hasclass('show')){
+//         menu.removeClass('show');
+//     } else{
+//         menu.addClass('show')
+//     }
+    
+// });
